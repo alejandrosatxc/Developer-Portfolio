46afd5c1b0943571d4b9f7a1ac06afff3c634de5
Alejandro Zapien <alejandro@satxconsultants.com>
2023-02-17 12:51:34 -0600

    escape apostrphe

--- pages/index.tsx
+++ pages/index.tsx
@@ -65,7 +65,7 @@ const Hero = () => {
         <div className="flex flex-row h-12">
           {last_name.map((letter, index) => <span key={index} className={`relative text-6xl hover:text-teal-300 text-white hover:text-7xl cursor-default transition-all animate-fade-in-down ${delays[index]} opacity-0 font-russo`}>{letter}</span>)}
         </div>
-        <p className={`${active ? 'opacity-100' : 'opacity-0'} duration-1000 delay-3000 transition-all md:w-1/2 mt-10 w-auto text-slate-300 text-xl text-left font-nunito`}>I'm a <span className='text-teal-300'>software engineer</span> that specializes in building apps, websites, front-ends and back-ends. I <span className='text-teal-300'>deploy and maintain</span> them too. </p>
+        <p className={`${active ? 'opacity-100' : 'opacity-0'} duration-1000 delay-3000 transition-all md:w-1/2 mt-10 w-auto text-slate-300 text-xl text-left font-nunito`}>I&aposm a <span className='text-teal-300'>software engineer</span> that specializes in building apps, websites, front-ends and back-ends. I <span className='text-teal-300'>deploy and maintain</span> them too. </p>
       </section>
       <div className={`${active ? 'opacity-100' : 'opacity-0'} duration-1000 delay-4000 transition-all animate-bounce text-white text-center w-18 h-8 bottom-4 -ml-9 left-1/2 fixed`}>
         <FontAwesomeIcon className="text-teal-400" icon={faArrowDown} size={'2x'} />
