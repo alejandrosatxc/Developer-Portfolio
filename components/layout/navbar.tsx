27e9433f3ee72fd0446132c13d0037cea2c5aef4
Alejandro Zapien <alejandro@satxconsultants.com>
2023-02-17 20:32:43 -0600

    Different color scheme

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -28,21 +28,21 @@ export default function Navbar() {
         <header>
             <nav className="bg-black md:h-24 h-[12vh] fixed z-50 w-full">
                 <div className="flex flex-row justify-between place-items-center w-full h-full px-6">
-                    <Link href="/"><h1 className="text-red-500 text-3xl animate-fade-in-down animation-delay-[3900ms] opacity-0">AZ</h1></Link>
+                    <Link href="/"><h1 className="text-teal-300 text-3xl animate-fade-in-down animation-delay-[3900ms] opacity-0">AZ</h1></Link>
                     <ul className="hidden md:flex h-full flex-row justify-between place-items-center">
                         {
                             menuItems.map((item, index) => {
                                 return (
-                                    <li key={item.title} className={`${router.pathname === item.path ? 'text-red-500 border-b-2 border-red-500 shadow-neon' : 'text-red-900'} animate-fade-in-down ${delays[index]} opacity-0 transition hover:shadow-neon duration-300 hover:border-b-2 hover:border-red-500 hover:text-red-500 flex justify-center place-items-center text-2xl font-nunito h-full mx-8`}>
+                                    <li key={item.title} className={`${router.pathname === item.path ? 'text-teal-300 border-b-2 border-teal-300 shadow-neon' : 'text-teal-900'} animate-fade-in-down ${delays[index]} opacity-0 transition hover:shadow-neon duration-300 hover:border-b-2 hover:border-red-500 hover:text-red-500 flex justify-center place-items-center text-2xl font-nunito h-full mx-8`}>
                                         <Link className={`h-full flex place-items-center`} href={item.path}>{item.title}</Link>
                                     </li>
                                 )
                             })
                         }
                     </ul>
-                    <Link href="https://github.com/alejandrosatxc"><FontAwesomeIcon className="text-red-500 text-4xl animate-fade-in-down opacity-0 animation-delay-[4400ms]" icon={faGithub} /></Link>
+                    <Link href="https://github.com/alejandrosatxc"><FontAwesomeIcon className="text-teal-300 text-4xl animate-fade-in-down opacity-0 animation-delay-[4400ms]" icon={faGithub} /></Link>
                     <div className="md:hidden">
-                        <FontAwesomeIcon onClick={() => { setMenuToggle(!menuToggle) }} className="animate-fade-in-down animation-delay-[4500ms] opacity-0 text-red-500 text-3xl" icon={faBars} />
+                        <FontAwesomeIcon onClick={() => { setMenuToggle(!menuToggle) }} className="animate-fade-in-down animation-delay-[4500ms] opacity-0 text-teal-300 text-3xl" icon={faBars} />
                     </div>
                 </div>
                 <div className={`${menuToggle ? 'animate-slide-in-left' : 'hidden'} z-50 fixed flex flex-col h-[88vh] right-0 backdrop-blur-md bg-white/30 w-64`}>
